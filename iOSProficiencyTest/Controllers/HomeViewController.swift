60e07b8599a07da71197ccc8b99009317911b20c
vishal <vishal.p@borqs.com>
2020-03-15 15:27:23 +0530

    Initial Project Setup -- Added RootView for ios 12 and 13 -- Main.StoryBoard removed from Project -- Update Project Setting -- update Info.plist

--- iOSProficiencyTest/Controllers/HomeViewController.swift
+++ iOSProficiencyTest/Controllers/HomeViewController.swift
@@ -2,13 +2,13 @@
 //  ViewController.swift
 //  iOSProficiencyTest
 //
-//  Created by Borqs India on 15/03/20.
+//  Created by VishalP on 15/03/20.
 //  Copyright © 2020 Vishal. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
