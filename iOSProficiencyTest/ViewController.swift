4e9b622818e61174f55636486e41f2675005df4a
vishal <vishal.p@borqs.com>
2020-03-15 15:08:26 +0530

    Initial Commit

---
+++ iOSProficiencyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSProficiencyTest
+//
+//  Created by Borqs India on 15/03/20.
+//  Copyright © 2020 Vishal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
