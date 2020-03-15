ad2f115cc9e4f1016cb38c7ac8e17710f9f2ce2a
vishal <vishal.p@borqs.com>
2020-03-15 15:40:51 +0530

    Set Initial ViewControllerUI  and TableView with Delagate

--- iOSProficiencyTest/Controllers/HomeViewController.swift
+++ iOSProficiencyTest/Controllers/HomeViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    //MARK:- UI Compenents
+    var tableInfoList = UITableView()
+    fileprivate let cellId = "cellId"
+    
+    //MARK:- Data Variables
+    var arrayInfoList = [AnyObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    //MARK:- Set UI for views
+    func setUpUIForViews() -> Void {
+        let refresh = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.refresh, target: self, action: #selector(fetchDataFromAPI))
+        self.navigationItem.rightBarButtonItem  = refresh
+        view.backgroundColor = .white
+        tableInfoList.delegate = self
+        tableInfoList.dataSource = self
+        tableInfoList.frame = view.frame
+        tableInfoList.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        view.addSubview(tableInfoList)
+    }
+}
 
-
+extension HomeViewController:UITableViewDelegate,UITableViewDataSource{
+    
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayInfoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableInfoList.dequeueReusableCell(withIdentifier: cellId) as! InfoTableViewCell
+        let obj = arrayInfoList[indexPath.row]
+        cell.setData(data: obj)
+        return cell
+    }
+    
 }
 
